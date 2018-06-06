802f8906139b065ff8ddbba6c3aad20a33618259
unknown <volodymyr.derecha@eleks.com>
2018-06-06 23:11:15 +0300

    setup android

--- App.js
+++ App.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import firebase from 'react-native-firebase';
-import config from 'config';
+import config from './config';
+
+firebase.initializeApp(
+  {
+    apiKey: config.FIREBASE_API_KEY,
+    databaseURL: config.FIREBASE_DATABASE_URL,
+    projectId: config.FIREBASE_PROJECT_ID,
+    persistence: true,
+  }
+);
 
 export default class App extends React.Component {
   componentDidMount() {
+    debugger;
     return firebase.auth()
       .signInAndRetrieveDataWithEmailAndPassword(config.EMAIL, config.PASSWORD)
       .then(response => {
         const authUser = response.user;
-        const ref = firebase.database().ref('/routes1/11');
+        const ref = firebase.database().ref('/routes/11');
         ref.keepSynced(true);
+        ref.once().then(v => {
+
+        });
       });
   }
 
